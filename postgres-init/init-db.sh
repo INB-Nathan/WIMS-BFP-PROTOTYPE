13104b3776640b49ab5e50bff0571eba40dd2aaa
xynate <202312375@fit.edu.ph>
2026-03-11 22:40:57 +0800

    feat(infra): finalize docker orchestration, repair pg permissions, and verify nginx gateway routing

--- postgres-init/init-db.sh
+++ postgres-init/init-db.sh
@@ -5,4 +5,6 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER keycloak WITH PASSWORD 'secret';
     CREATE DATABASE keycloak;
     GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+    \c keycloak
+    GRANT ALL ON SCHEMA public TO keycloak;
 EOSQL
